cc309b9ff4af2c1005c007ba4ea7654f065c91fd
fpmystar <fpmystar@gmail.com>
2011-06-19 20:22:56 +0800

    fp testbranch merge the uarttest.cc
    Signed-off-by: fpmystar <fpmystar@gmail.com>

--- uarttest.cc
+++ uarttest.cc
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-    ofstream uart_console;
-cout<<"grass add this"<<endl;
-    uart_console.open("/tmp/mylinuxfifo");
+    	ofstream uart_console;
+	cout<<"grass add this"<<endl;
+	cout<<"fp testbranch"<<endl;
+    	uart_console.open("/tmp/mylinuxfifo");  // fp add this
 	if(!uart_console)
 		cerr << "uart_console open failse" << endl;
 	uart_console<<"dsafsadfasdfas";
