e91c695b49a7a7f737f6e8672ed9b999274cd1d6
grass <zqq1988@gmail.com>
2011-06-19 20:17:42 +0800

    grass-test modify uarttest.cc
    Signed-off-by: grass <zqq1988@gmail.com>

--- uarttest.cc
+++ uarttest.cc
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     ofstream uart_console;
+cout<<"grass add this"<<endl;
     uart_console.open("/tmp/mylinuxfifo");
 	if(!uart_console)
 		cerr << "uart_console open failse" << endl;
